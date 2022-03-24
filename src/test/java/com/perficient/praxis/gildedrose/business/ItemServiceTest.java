fdcb294d650a02a7f3ad8619528a1c0860d9b568
juangh15 <47037988+juangh15@users.noreply.github.com>
2022-03-24 10:23:17 -0500

    First tests of ItemService

--- src/test/java/com/perficient/praxis/gildedrose/business/ItemServiceTest.java
+++ src/test/java/com/perficient/praxis/gildedrose/business/ItemServiceTest.java
@@ -68,6 +68,131 @@ public class ItemServiceTest {
         verify(itemRepository,times(1)).save(any());
     }
 
+    @Test
+    /**
+     * GIVEN a valid normal type item in the database with sellin value less than 0
+     * WHEN updateQuality method is called
+     * THEN the service should update the quality decreasing it by 1
+     * and update sellIn decreasing it by 2
+     */
+    public void testUpdateQualityOfNormalTypeItemSellInPassed(){
+        var item = new Item(0, "Cookie", -1, 30, Item.Type.NORMAL);
+        when(itemRepository.findAll()).thenReturn(List.of(item));
+
+        List<Item> itemsUpdated = itemService.updateQuality();
+
+        assertEquals(0, itemsUpdated.get(0).getId());
+        assertEquals("Cookie", itemsUpdated.get(0).name);
+        assertEquals(-2, itemsUpdated.get(0).sellIn);
+        assertEquals(28, itemsUpdated.get(0).quality);
+        assertEquals(Item.Type.NORMAL, itemsUpdated.get(0).type);
+        verify(itemRepository,times(1)).save(any());
+    }
+
+    @Test
+    /**
+     * GIVEN a valid normal type item in the database with quality set in 0
+     * WHEN updateQuality method is called
+     * THEN the service should not decrease the quality
+     */
+    public void testUpdateQualityOfNormalTypeItemQualityNeverNegative(){
+        var item = new Item(0, "Cookie", 2, 0, Item.Type.NORMAL);
+        when(itemRepository.findAll()).thenReturn(List.of(item));
+
+        List<Item> itemsUpdated = itemService.updateQuality();
+
+        assertEquals(0, itemsUpdated.get(0).getId());
+        assertEquals("Cookie", itemsUpdated.get(0).name);
+        assertEquals(1, itemsUpdated.get(0).sellIn);
+        assertEquals(0, itemsUpdated.get(0).quality);
+        assertEquals(Item.Type.NORMAL, itemsUpdated.get(0).type);
+        verify(itemRepository,times(1)).save(any());
+    }
+
+    @Test
+    /**
+     * GIVEN a valid AGED type item in the database
+     * WHEN updateQuality method is called
+     * THEN the service should decrease the sellin by 1 and
+     * increase the quality by 1
+     */
+    public void testUpdateQualityOfAgedTypeItem(){
+        var item = new Item(0, "Cookie", 3, 49, Item.Type.AGED);
+        when(itemRepository.findAll()).thenReturn(List.of(item));
+
+        List<Item> itemsUpdated = itemService.updateQuality();
+
+        assertEquals(0, itemsUpdated.get(0).getId());
+        assertEquals("Cookie", itemsUpdated.get(0).name);
+        assertEquals(2, itemsUpdated.get(0).sellIn);
+        assertEquals(50, itemsUpdated.get(0).quality);
+        assertEquals(Item.Type.AGED, itemsUpdated.get(0).type);
+        verify(itemRepository,times(1)).save(any());
+    }
+
+    @Test
+    public void testUpdateQualityOfTicketTypeItemWithSellInLessThanEleven(){
+        var item = new Item(0, "Jamming", 10, 10, Item.Type.TICKETS);
+        when(itemRepository.findAll()).thenReturn(List.of(item));
+
+        List<Item> itemsUpdated = itemService.updateQuality();
+
+        assertEquals(0, itemsUpdated.get(0).getId());
+        assertEquals("Jamming", itemsUpdated.get(0).name);
+        assertEquals(9, itemsUpdated.get(0).sellIn);
+        assertEquals(12, itemsUpdated.get(0).quality);
+        assertEquals(Item.Type.TICKETS, itemsUpdated.get(0).type);
+        verify(itemRepository,times(1)).save(any());
+    }
+
+    @Test
+    public void testUpdateQualityOfTicketTypeItemWithSellInLessThanSix(){
+        var item = new Item(0, "Jamming", 5, 10, Item.Type.TICKETS);
+        when(itemRepository.findAll()).thenReturn(List.of(item));
+
+        List<Item> itemsUpdated = itemService.updateQuality();
+
+        assertEquals(0, itemsUpdated.get(0).getId());
+        assertEquals("Jamming", itemsUpdated.get(0).name);
+        assertEquals(4, itemsUpdated.get(0).sellIn);
+        assertEquals(13, itemsUpdated.get(0).quality);
+        assertEquals(Item.Type.TICKETS, itemsUpdated.get(0).type);
+        verify(itemRepository,times(1)).save(any());
+    }
+
+    @Test
+    public void testUpdateQualityOfTicketTypeItemSellInZero(){
+        var item = new Item(0, "Jamming", 0, 10, Item.Type.TICKETS);
+        when(itemRepository.findAll()).thenReturn(List.of(item));
+
+        List<Item> itemsUpdated = itemService.updateQuality();
+
+        assertEquals(0, itemsUpdated.get(0).getId());
+        assertEquals("Jamming", itemsUpdated.get(0).name);
+        assertEquals(-1, itemsUpdated.get(0).sellIn);
+        assertEquals(0, itemsUpdated.get(0).quality);
+        assertEquals(Item.Type.TICKETS, itemsUpdated.get(0).type);
+        verify(itemRepository,times(1)).save(any());
+    }
+
+    @Test
+    public void testUpdateQualityOfLegendaryTypeItem(){
+        var item = new Item(0, "Potion", 5, 10, Item.Type.LEGENDARY);
+        when(itemRepository.findAll()).thenReturn(List.of(item));
+
+        List<Item> itemsUpdated = itemService.updateQuality();
+
+        assertEquals(0, itemsUpdated.get(0).getId());
+        assertEquals("Potion", itemsUpdated.get(0).name);
+        assertEquals(5, itemsUpdated.get(0).sellIn);
+        assertEquals(10, itemsUpdated.get(0).quality);
+        assertEquals(Item.Type.LEGENDARY, itemsUpdated.get(0).type);
+        verify(itemRepository,times(1)).save(any());
+    }
+
+
+
+
 
 
 
